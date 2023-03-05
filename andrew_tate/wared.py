916677797cf7cbf10d10b5da96aaad28f5166308
Андрей Пожиленков <andrejpozilenkov@MacBook-Air-Andrej.local>
2023-03-05 16:59:36 +0200

    andrew_tate_test1

---
+++ andrew_tate/wared.py
@@ -0,0 +1,27 @@
+def sum(a, b):
+    c = a + b
+    return c
+
+def sup(a, b):
+    c = a - b
+    return c
+
+def dev(a, b):
+    c = a / b
+    return c
+
+def mult(a, b):
+    c = a * b
+    return c
+
+a = sum(5, 8)
+print(a)
+
+b = sup(b=4, a=8)
+print(b)
+
+c = dev(b=8, a=4)
+print(c)
+
+d = mult(5, 5)
+print(d)
