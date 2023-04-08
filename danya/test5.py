b1084d68d0b22c61e4e725b76d7b016997c89399
danya06 <danyaozanyan@gmail.com>
2023-04-08 15:32:41 +0300

    переписывали рекурсивные функции в цикл

--- danya/test5.py
+++ danya/test5.py
@@ -1,4 +1,3 @@
-n = -3
 def fact(n):
     if n==1:
         return 1
@@ -6,9 +5,14 @@ def fact(n):
         return None
     return n*fact(n-1)
 
-print(fact(n))
-print('_____')
-a = 3
+def fact2(b):
+    index = 1
+    res = 1
+    while index <= b:
+        res = res*index
+        index += 1
+    return res
+
 def Fibonacci(a):
     if a < 0:
         return None
@@ -18,4 +22,28 @@ def Fibonacci(a):
         return 1
     if a>=2:
         return Fibonacci(a-1)+Fibonacci(a-2)
+    
+def Fib(c):
+    if c==0:
+        return 0
+    if c==1:
+        return 1
+    index = 1
+    f_n = 1
+    f_n_1 = 0
+    while index<c:
+        f_n = f_n + f_n_1
+        f_n_1 = f_n - f_n_1
+        index += 1
+        res = f_n
+    return f_n
+
+print(fact(3))
+print('____')
+print(fact2(5))
+print('____')
+a = 5
 print(Fibonacci(a))
+print('____')
+c = 100
+print(Fib(c))
